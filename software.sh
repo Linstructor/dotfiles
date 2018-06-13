ac80cc42401bc4d00d6f0fb4866ead65039f83a8
Linsctructor <tristan.morisot@viacesi.fr>
2018-06-13 21:18:14 +0200

    adding steam

--- software.sh
+++ software.sh
@@ -48,3 +48,5 @@ nvm install node
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt install steam
